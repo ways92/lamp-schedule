34b8c2a7233034436b2092ea05a672e0b3678237
ways92 <wayslos92@gmail.com>
2025-02-18 20:07:35 +0700

    fixing authOption

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,63 +1,65 @@
-import NextAuth, { AuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcrypt";
-import { prisma } from "@/lib/prisma";
+import NextAuth from "next-auth";
+import { authOptions } from "@/utils/authOptions";
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import bcrypt from "bcrypt";
+// import { prisma } from "@/lib/prisma";
 
-// 7 hari dalam detik
-const expiredToken = 7 * 60 * 60 * 24;
+// // 7 hari dalam detik
+// const expiredToken = 7 * 60 * 60 * 24;
 
-export const authOptions: AuthOptions = {
-  session: {
-    strategy: "jwt", // Gunakan "jwt" sebagai nilai yang valid
-    maxAge: expiredToken,
-  },
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "text" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error("Invalid email or password");
-        }
+// export const authOptions: AuthOptions = {
+//   session: {
+//     strategy: "jwt", // Gunakan "jwt" sebagai nilai yang valid
+//     maxAge: expiredToken,
+//   },
+//   providers: [
+//     CredentialsProvider({
+//       name: "Credentials",
+//       credentials: {
+//         email: { label: "Email", type: "text" },
+//         password: { label: "Password", type: "password" },
+//       },
+//       async authorize(credentials) {
+//         if (!credentials?.email || !credentials?.password) {
+//           throw new Error("Invalid email or password");
+//         }
 
-        const user = await prisma.user.findUnique({
-          where: { email: credentials.email },
-        });
+//         const user = await prisma.user.findUnique({
+//           where: { email: credentials.email },
+//         });
 
-        if (!user) {
-          throw new Error("User not found");
-        }
+//         if (!user) {
+//           throw new Error("User not found");
+//         }
 
-        const isValid = await bcrypt.compare(credentials.password, user.password);
-        if (!isValid) {
-          throw new Error("Invalid password");
-        }
+//         const isValid = await bcrypt.compare(credentials.password, user.password);
+//         if (!isValid) {
+//           throw new Error("Invalid password");
+//         }
 
-        return user;
-      },
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {
-    async session({ session, token }) {
-      session.user.id = token.sub as string;
-      return session;
-    },
-    async jwt({ token, user }) {
-      if (user) {
-        token.sub = user.id;
-        token.exp = Math.floor(Date.now() / 1000) + expiredToken; // Expire dalam 7 hari
-      }
-      return token;
-    },
-  },
-  pages: {
-    signIn: "/auth/login",
-  },
-};
+//         return user;
+//       },
+//     }),
+//   ],
+//   secret: process.env.NEXTAUTH_SECRET,
+//   callbacks: {
+//     async session({ session, token }) {
+//       session.user.id = token.sub as string;
+//       return session;
+//     },
+//     async jwt({ token, user }) {
+//       if (user) {
+//         token.sub = user.id;
+//         token.exp = Math.floor(Date.now() / 1000) + expiredToken; // Expire dalam 7 hari
+//       }
+//       return token;
+//     },
+//   },
+//   pages: {
+//     signIn: "/auth/login",
+//   },
+// };
 
 // Ekspor default
-export default NextAuth(authOptions);
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
---
+++ src/utils/authOptions.ts
@@ -0,0 +1,60 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import { prisma } from "@/lib/prisma";
+import { NextAuthOptions } from "next-auth";
+
+// 7 hari dalam detik
+const expiredToken = 7 * 60 * 60 * 24;
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt", // Gunakan "jwt" sebagai nilai yang valid
+    maxAge: expiredToken,
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid email or password");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user) {
+          throw new Error("User not found");
+        }
+
+        const isValid = await bcrypt.compare(credentials.password, user.password);
+        if (!isValid) {
+          throw new Error("Invalid password");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session, token }) {
+      session.user.id = token.sub as string;
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.sub = user.id;
+        token.exp = Math.floor(Date.now() / 1000) + expiredToken; // Expire dalam 7 hari
+      }
+      return token;
+    },
+  },
+  pages: {
+    signIn: "/auth/login",
+  },
+};
